cba014ab1a22d731dd07de7c0b368a36204f1a4e
shironegii <47374565+locomorange@users.noreply.github.com>
2025-07-17 09:25:48 +0000

    fix: CMakeインストールプレフィックスを削除

--- scripts/build-brisk-full.sh
+++ scripts/build-brisk-full.sh
@@ -61,14 +61,11 @@ cmake -G Ninja \
     -S . \
     -B build \
     -DVCPKG_MANIFEST_INSTALL=ON \
-    -DCMAKE_INSTALL_PREFIX=dist \
     -DVCPKG_TARGET_TRIPLET=${BRISK_TRIPLET} \
     -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake \
     -DVCPKG_INSTALLED_DIR=../vcpkg/installed \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-
-#    
 
 # cmake --build build --target install
 
